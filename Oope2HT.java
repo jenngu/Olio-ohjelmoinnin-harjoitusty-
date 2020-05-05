d52616d0c638d79b5dc211c5a50a7c93b6cd81c5
Jennifer Nguyen <jennifer.nguyen@tuni.fi>
2020-05-05 18:46:06 +0300

    Initial commit

---
+++ Oope2HT.java
@@ -0,0 +1,17 @@
+
+import harjoitustyo.toiminnot.Kayttoliittyma;
+
+/*
+ * Oopen ajoluokka, jossa kutsutaan Käyttöliittymä-luokkaa 
+ * <p>
+ * @author jennifernguyen
+ */
+public class Oope2HT {
+    public static void main(String[]args){
+        // Luodaan käyttöliittymä
+        Kayttoliittyma käyttöliittymä = new Kayttoliittyma();
+        // Suoritetaan käyttöliittymä
+        käyttöliittymä.suorita();
+        
+    }
+}
