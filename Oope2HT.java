0f5b7aaa051e454baa21fa79e8a89ff70734d91f
Jennifer Nguyen <jennifer.nguyen@tuni.fi>
2021-02-07 19:26:51 +0200

    Initial commit.

--- Oope2HT.java
+++ Oope2HT.java
@@ -3,12 +3,7 @@ import harjoitustyo.toiminnot.Kayttoliittyma;
 
 /*
  * Harjoitustyön ajoluokka, jossa kutsutaan Käyttöliittymä-luokkaa 
- * <p>
  * Harjoitustyö, Olio-ohjelmoinnin perusteet II, 2020
- * <p>  
- * @author jennifernguyen, (jennifer.nguyen@tuni.fi)
- * Informaatioteknologian ja viestinnän tiedekunta,
- * Tampereen yliopisto
  */
 public class Oope2HT {
     public static void main(String[]args){
